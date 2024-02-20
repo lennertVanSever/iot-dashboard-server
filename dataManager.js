a9eb4811e136a202851defa02057dfcdb9cff139
Lennert Van Sever <lennert.van-sever@weareretail.ai>
2024-02-20 15:23:56 +0100

    cleanup

--- dataManager.js
+++ dataManager.js
@@ -1,64 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-const { faunaClient, q } = require('./faunaDb');
-const dataFilePath = path.join(__dirname, 'data.json');
+const { faunaClient, q } = require("./faunaDb");
 
 function isValidRecord(record) {
-    return record &&
-        record.timestamp &&
-        record.data &&
-        typeof record.data.location === 'string' &&
-        typeof record.data.device_name === 'string' &&
-        typeof record.data.battery_voltage === 'number' &&
-        !isNaN(parseFloat(record.data.temperature)) &&
-        !isNaN(parseFloat(record.data.pressure));
+  return (
+    record &&
+    record.timestamp &&
+    record.data &&
+    typeof record.data.location === "string" &&
+    typeof record.data.device_name === "string" &&
+    typeof record.data.battery_voltage === "number" &&
+    !isNaN(parseFloat(record.data.temperature)) &&
+    !isNaN(parseFloat(record.data.pressure))
+  );
 }
 
-async function appendMessageToDataFile(record, callback) {
-    fs.readFile(dataFilePath, async (err, data) => {
-        let messages = [];
-        if (!err && data.length) {
-            try {
-                messages = JSON.parse(data.toString());
-            } catch (parseError) {
-                console.error('Error parsing JSON from data file:', parseError);
-                return;
-            }
-        }
-
-        if (isValidRecord(record)) {
-            messages.push(record);
-
-            // Attempt to write to FaunaDB
-            try {
-                const insertResult = await faunaClient.query(
-                    q.Create(
-                        q.Collection('sensors'),
-                        record
-                    )
-                );
-                console.log('FaunaDB Insert Result:', insertResult);
-                if (callback) callback();
-            } catch (faunaError) {
-                console.error('FaunaDB Insert Error:', faunaError);
-            }
-
-            if (false) {
-                fs.writeFile(dataFilePath, JSON.stringify(messages, null, 2), (writeError) => {
-                    if (writeError) {
-                        console.error('Error writing to data file:', writeError);
-                    } else {
-                        console.log('Record appended to data.json');
-                        if (callback) callback();
-                    }
-                });
-            }
-        } else {
-            console.error('Received invalid data. Not saving to file.', record);
-        }
-    });
+async function insertRecordFaunaDB(record, callback) {
+  if (isValidRecord(record)) {
+    // Attempt to write to FaunaDB
+    try {
+      const insertResult = await faunaClient.query(
+        q.Create(q.Collection("sensors"), record)
+      );
+      console.log("FaunaDB Insert Result:", insertResult);
+      if (callback) callback();
+    } catch (faunaError) {
+      console.error("FaunaDB Insert Error:", faunaError);
+    }
+  } else {
+    console.error("Received invalid data.", record);
+  }
 }
 
 module.exports = {
-    appendMessageToDataFile,
+  insertRecordFaunaDB,
 };
--- mqttClient.js
+++ mqttClient.js
@@ -1,35 +1,35 @@
-const mqtt = require('mqtt');
-const appendMessageToDataFile = require('./dataManager').appendMessageToDataFile;
+const mqtt = require("mqtt");
+const { insertRecordFaunaDB } = require("./dataManager");
 
 function setupMqttClient(io) {
-    const client = mqtt.connect('mqtt://broker.emqx.io');
+  const client = mqtt.connect("mqtt://broker.emqx.io");
 
-    client.on('connect', () => {
-        console.log('Connected to MQTT Broker');
-        client.subscribe('ehbIoT/sensordata', (err) => {
-            if (!err) {
-                console.log('Subscribed to the topic: ehbIoT/sensordata');
-            } else {
-                console.error('Failed to subscribe:', err);
-            }
-        });
+  client.on("connect", () => {
+    console.log("Connected to MQTT Broker");
+    client.subscribe("ehbIoT/sensordata", (err) => {
+      if (!err) {
+        console.log("Subscribed to the topic: ehbIoT/sensordata");
+      } else {
+        console.error("Failed to subscribe:", err);
+      }
     });
+  });
 
-    client.on('message', (topic, message) => {
-        console.log(`Message received on topic ${topic}: ${message.toString()}`);
-        const record = {
-            timestamp: new Date().toISOString(),
-            data: JSON.parse(message.toString()),
-        };
+  client.on("message", (topic, message) => {
+    console.log(`Message received on topic ${topic}: ${message.toString()}`);
+    const record = {
+      timestamp: new Date().toISOString(),
+      data: JSON.parse(message.toString()),
+    };
 
-        appendMessageToDataFile(record, () => {
-            io.emit('sensorData', record);
-        });
+    insertRecordFaunaDB(record, () => {
+      io.emit("sensorData", record);
     });
+  });
 
-    client.on('error', (err) => {
-        console.log('Error encountered:', err);
-    });
+  client.on("error", (err) => {
+    console.log("Error encountered:", err);
+  });
 }
 
 module.exports = setupMqttClient;
